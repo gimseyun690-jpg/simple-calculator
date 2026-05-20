2c4f993cf63abdf37ecac258fa0215f7d9d14379
Kimseyun <gimseyun690@gmail.com>
2026-05-20 08:46:29 +0000

    feat: 곱하기 연산 추가

--- tests/unit/calculator.test.js
+++ tests/unit/calculator.test.js
@@ -1,6 +1,14 @@
 // 단위 테스트: 함수 하나를 격리해서 테스트
-import { add, calculate, subtract } from "../../src/calculator.js";
-
+import { add, subtract, calculate } from "../../src/calculator.js";
+// (기존 describe들 그대로 두고, 아래 블록만 추가)
+describe("subtract", () => {
+ test("두 수를 뺀다", () => {
+ expect(subtract(10, 4)).toBe(6);
+ });
+ test("음수 결과도 처리한다", () => {
+ expect(subtract(3, 10)).toBe(-7);
+ });
+});
 describe("add", () => {
   test("두 양수를 더한다", () => {
     expect(add(2, 3)).toBe(5);
@@ -11,23 +19,9 @@ describe("add", () => {
   });
 });
 
-describe("subtract", () => {
-  test("두 수를 뺀다", () => {
-    expect(subtract(10, 4)).toBe(6);
-  });
-
-  test("결과가 음수여도 처리한다", () => {
-    expect(subtract(3, 8)).toBe(-5);
-  });
-});
-
 describe("calculate", () => {
   test("연산자 문자열로 알맞은 함수를 호출한다", () => {
     expect(calculate("add", 1, 2)).toBe(3);
-  });
-
-  test("subtract 연산자 문자열로 빼기를 호출한다", () => {
-    expect(calculate("subtract", 7, 2)).toBe(5);
   });
 
   test("알 수 없는 연산자는 에러를 던진다", () => {
